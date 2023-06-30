164850a8d02256dc3a39c67c11b20024da8fd021
Cesar Ogalde H <usradmt2@baccredomatic.cr>
2023-06-29 18:24:29 -0600

    mejoras

--- BAC/DPA/plan.md
+++ BAC/DPA/plan.md
@@ -31,6 +31,9 @@ OK 4.2.- Diagrama Levantamiento de políticas (Ambientes, Ramas, Aprobadores).
 5.1.- (1sem) Documentación pipeline de creación Proyectos Ansibles según buenas prácticas.
 5.2.- (1sem) Definición y estructuras de documentación (MarkDown) y Validación en pipeline.
 5.3.- (1sem) Incorporar Tarea de Validación de código en template pipeline.
+             - el uso de ansible-lint (On RHEL, ansible-lint package is part of "Red Hat Ansible Automation Platform" subscription, which needs to be activated.)
+             - la otra alternativa es usar pip y librería abierta pero da error en gcc (pip3 install ansible-lint)
+             Command "python setup.py egg_info" failed with error code 1 in /tmp/pip-build-bppb8vl1/ruamel.yaml.clib/
 5.4.- (1sem) Agregar Environment's con restricción de ramas, templates.
 5.5.- (1sem) Agregar pruebas unitarias de desarrollo al pipeline
 5.6.- (1sem) Complementar requerimientos generales heredados con los específicos de Ansible, sobre todo en autorizaciones
--- movemouse/MouseMover.java
+++ movemouse/MouseMover.java
@@ -1,16 +1,22 @@
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
-import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MouseMover {
     public static final int FIVE_SECONDS = 5000;
-    public static final int MAX_Y = 400;
-    public static final int MAX_X = 400;
+    private static final Logger logger = Logger.getLogger(MouseMover.class.getName());
 
     public static void main(String... args) throws Exception {
         Robot robot = new Robot();
-        Random random = new Random();
         while (true) {
-            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            Point pObj = MouseInfo.getPointerInfo().getLocation();
+            robot.mouseMove(pObj.x + 1, pObj.y + 1);
+            robot.mouseMove(pObj.x - 1, pObj.y - 1);
+            pObj = MouseInfo.getPointerInfo().getLocation();
+            logger.log(Level.INFO, pObj.toString() + "x>>" + pObj.x + "  y>>" + pObj.y);
+            //robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
             Thread.sleep(FIVE_SECONDS);
         }
     }
