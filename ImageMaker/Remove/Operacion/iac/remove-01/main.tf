57924c0a52eb7f18eebdcfebadec49478b99d10b
Cesar Ogalde H <cesar.ogalde@imagemaker.com>
2023-09-11 11:20:45 -0300

    Agrega Cloud Front

--- ImageMaker/Remove/Operacion/iac/remove-01/main.tf
+++ ImageMaker/Remove/Operacion/iac/remove-01/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
@@ -13,7 +12,6 @@ provider "aws" {
   access_key = var.dev_access_key
   secret_key = var.dev_secret_key
   region  = var.dev_region
-
   default_tags {
     tags = {
       proyecto     = "remove-01"
@@ -159,5 +157,209 @@ resource "aws_ecs_cluster" "produccion" {
     }
     service_connect_defaults {
         namespace = "arn:aws:servicediscovery:us-east-1:253021683072:namespace/ns-gsyxx3amncpkavp4"
+    }
+}
+
+# aws_cloudfront_distribution.app_remove_group:
+resource "aws_cloudfront_distribution" "app_remove_group" {
+    aliases                        = [
+        "app.removegroup.com",
+    ]
+    comment                        = "Produccion"
+    default_root_object            = "index.html"
+    enabled                        = true
+    http_version                   = "http2"
+    is_ipv6_enabled                = true
+    price_class                    = "PriceClass_All"
+    retain_on_delete               = false
+    tags = {
+        "environment" = "prd"
+    }
+    wait_for_deployment            = true
+    web_acl_id                     = "arn:aws:wafv2:us-east-1:253021683072:global/webacl/CreatedByCloudFront-d3a3523c-d1ca-4fcd-84db-ff964e8632a5/df7e29f8-2a1f-4226-a19d-5e4c65d13826"
+
+    default_cache_behavior {
+        allowed_methods        = [
+            "GET",
+            "HEAD",
+        ]
+        cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+        cached_methods         = [
+            "GET",
+            "HEAD",
+        ]
+        compress               = true
+        default_ttl            = 0
+        max_ttl                = 0
+        min_ttl                = 0
+        smooth_streaming       = false
+        target_origin_id       = "removeapp-prod.s3.us-east-1.amazonaws.com"
+        trusted_key_groups     = []
+        trusted_signers        = []
+        viewer_protocol_policy = "redirect-to-https"
+    }
+
+    origin {
+        connection_attempts = 3
+        connection_timeout  = 10
+        domain_name         = "removeapp-prod.s3.us-east-1.amazonaws.com"
+        origin_id           = "removeapp-prod.s3.us-east-1.amazonaws.com"
+    }
+
+    restrictions {
+        geo_restriction {
+            locations        = []
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        acm_certificate_arn            = "arn:aws:acm:us-east-1:253021683072:certificate/222f1b33-847b-4a79-ac56-2f305384b809"
+        cloudfront_default_certificate = false
+        minimum_protocol_version       = "TLSv1.2_2021"
+        ssl_support_method             = "sni-only"
+    }
+}
+
+# aws_cloudfront_distribution.qa_remove_group:
+resource "aws_cloudfront_distribution" "qa_remove_group" {
+    aliases                        = [
+        "qa.removegroup.com",
+    ]
+    comment                        = "QA"
+    default_root_object            = "index.html"
+    enabled                        = true
+    http_version                   = "http1.1"
+    is_ipv6_enabled                = true
+    price_class                    = "PriceClass_100"
+    retain_on_delete               = false
+    tags = {
+        "environment" = "dev"
+    }
+    wait_for_deployment            = true
+    default_cache_behavior {
+        allowed_methods            = [
+            "DELETE",
+            "GET",
+            "HEAD",
+            "OPTIONS",
+            "PATCH",
+            "POST",
+            "PUT",
+        ]
+        cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+        cached_methods             = [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ]
+        compress                   = true
+        default_ttl                = 0
+        max_ttl                    = 0
+        min_ttl                    = 0
+        response_headers_policy_id = "60669652-455b-4ae9-85a4-c4c02393f86c"
+        smooth_streaming           = false
+        target_origin_id           = "bucket-s3"
+        trusted_key_groups         = []
+        trusted_signers            = []
+        viewer_protocol_policy     = "redirect-to-https"
+    }
+
+    logging_config {
+        bucket          = "removeapp-logs.s3.amazonaws.com"
+        include_cookies = true
+        prefix          = "CloudFront-logs"
+    }
+
+    origin {
+        connection_attempts = 3
+        connection_timeout  = 10
+        domain_name         = "removedevs3-0000.s3.us-east-1.amazonaws.com"
+        origin_id           = "bucket-s3"
+    }
+
+    restrictions {
+        geo_restriction {
+            locations        = []
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        acm_certificate_arn            = "arn:aws:acm:us-east-1:253021683072:certificate/222f1b33-847b-4a79-ac56-2f305384b809"
+        cloudfront_default_certificate = false
+        minimum_protocol_version       = "TLSv1.2_2021"
+        ssl_support_method             = "sni-only"
+    }
+}
+
+# aws_cloudfront_distribution.removesolutions:
+resource "aws_cloudfront_distribution" "removesolutions" {
+    aliases                        = [
+        "removesolutions.com",
+    ]
+    comment                        = "Prod -old"
+    default_root_object            = "index.html"
+    enabled                        = true
+    http_version                   = "http1.1"
+    is_ipv6_enabled                = true
+    price_class                    = "PriceClass_100"
+    retain_on_delete               = false
+    tags = {
+        "environment" = "dev"
+    }
+    wait_for_deployment            = true
+
+    default_cache_behavior {
+        allowed_methods        = [
+            "DELETE",
+            "GET",
+            "HEAD",
+            "OPTIONS",
+            "PATCH",
+            "POST",
+            "PUT",
+        ]
+        cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+        cached_methods         = [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ]
+        compress               = true
+        default_ttl            = 0
+        max_ttl                = 0
+        min_ttl                = 0
+        smooth_streaming       = false
+        target_origin_id       = "removesolutions.com"
+        trusted_key_groups     = []
+        trusted_signers        = []
+        viewer_protocol_policy = "redirect-to-https"
+    }
+
+    logging_config {
+        bucket          = "removeapp-logs.s3.amazonaws.com"
+        include_cookies = true
+        prefix          = "CloudFront-Prd"
+    }
+
+    origin {
+        connection_attempts = 3
+        connection_timeout  = 10
+        domain_name         = "remove-solution-temp.s3.us-east-1.amazonaws.com"
+        origin_id           = "removesolutions.com"
+    }
+
+    restrictions {
+        geo_restriction {
+            locations        = []
+            restriction_type = "none"
+        }
+    }
+    viewer_certificate {
+        acm_certificate_arn            = "arn:aws:acm:us-east-1:253021683072:certificate/92f48d25-7bd5-41dd-8dc4-d9dd77e7a22e"
+        cloudfront_default_certificate = false
+        minimum_protocol_version       = "TLSv1.2_2021"
+        ssl_support_method             = "sni-only"
     }
 }
