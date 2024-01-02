2a8790062643856dd6b8bea9b6193e2e5ab08b45
Cesar Ogalde H <cesar.ogalde@imagemaker.com>
2024-01-02 20:22:54 -0300

    Actualización

--- proyectos/FitIT/data-azure-demo/main.tf
+++ proyectos/FitIT/data-azure-demo/main.tf
@@ -76,7 +76,7 @@ resource "azurerm_network_security_group" "nsg_demo" {
     destination_address_prefix = "*"
   }
 
-    security_rule {
+  security_rule {
     name                       = "HTTP"
     priority                   = 1002
     direction                  = "Inbound"
@@ -87,7 +87,17 @@ resource "azurerm_network_security_group" "nsg_demo" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  
+   security_rule {
+    name                       = "SQLServer"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "10.0.2.0/32"
+    destination_address_prefix = "${azurerm_virtual_network.net_demo.address_space.0}"
+  } 
   depends_on = [ azurerm_resource_group.rg_demo ]
 }
 
@@ -134,49 +144,44 @@ resource "azurerm_windows_virtual_machine" "vm_demo" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-
- 
   depends_on = [ azurerm_network_interface_security_group_association.sg_nic_demo ]
 }
-/*
-resource "null_resource" "configure" {
-    provisioner "remote-exec" {
-        inline = [
-            "Invoke-WebRequest -Uri 'https://download.microsoft.com/download/7/0/9/709CBE7F-2005-4A83-B405-CA37A9AB8DC8/PowerBIReportServer.exe' -OutFile 'C:\\PowerBIReportServer.exe'",
-            "Start-Process -Wait -FilePath 'C:\\PowerBIReportServer.exe' -ArgumentList '/quiet /norestart'",
-        ]
-        connection {
-            type     = "winrm"
-            user     = "adminuser"
-            password = "P@$$w0rd1234!"
-            host     = "${azurerm_windows_virtual_machine.vm_demo.public_ip_address}"
-        }
-    }
-    depends_on = [ azurerm_windows_virtual_machine.vm_demo ]
-}
-*/
 
-resource "azurerm_mssql_server" "sql_server_demo" {
-  name                         = "sql-server-demo-v2"
-  resource_group_name          = azurerm_resource_group.rg_demo.name
-  location                     = azurerm_resource_group.rg_demo.location
-  version                      = "19.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "Password1234!"
+# tarjeta de red
+resource "azurerm_network_interface" "nic_sql_demo" {
+  name                = "nic-sql-demo"
+  location            = azurerm_resource_group.rg_demo.location
+  resource_group_name = azurerm_resource_group.rg_demo.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetdemo.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  depends_on = [ azurerm_resource_group.rg_demo, azurerm_subnet.subnetdemo ]
 }
 
-resource "azurerm_mssql_database" "db_sql_server_demo" {
-  name                        = "demo"
-  server_id                   = azurerm_mssql_server.sql_server_demo.id
-  collation                   = "SQL_Latin1_General_CP1_CI_AS"
-  license_type                = "LicenseIncluded"
-  #max_size_gb                 = 250
-  sku_name                    = "S0"
-  depends_on = [ azurerm_mssql_server.sql_server_demo ]
+resource "azurerm_windows_virtual_machine" "vm_sql_demo" {
+  name                = "vmsqldemo"
+  resource_group_name = azurerm_resource_group.rg_demo.name
+  location            = azurerm_resource_group.rg_demo.location
+  size                = "Standard_B2s"
+  admin_username      = "sqluser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic_sql_demo.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftSQLServer"
+    offer     = "sql2019-ws2019"
+    sku       = "SQLDEV-gen2"
+    version   = "latest"
+}
+  depends_on = [ azurerm_network_interface_security_group_association.sg_nic_demo ]
 }
 
-resource "azurerm_mssql_virtual_network_rule" "sqlvnetrule" {
-  name                = "sql-net-rule-demo"
-  server_id = azurerm_mssql_server.sql_server_demo.id
-  subnet_id = azurerm_subnet.subnetdemo.id
-}
